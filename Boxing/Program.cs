6f479f3e70a5d3c32af75744d2f8332c0be08ca8
Anton Szórád <anton.szorad@gmail.com>
2015-08-14 10:50:52 +0200

    Initial commit

---
+++ Boxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 42;
+
+            object o = i;
+
+            i = 43;
+
+            Console.WriteLine("Value typed int i: " + i);
+            Console.WriteLine("Object typed object o: " + o);
+            Console.ReadKey();
+        }
+    }
+}
