4b7117c86191268ad94897b3586eab2960a95a36
Anton Szórád <anton.szorad@gmail.com>
2015-08-14 11:06:28 +0200

    Added some easy stuff regarding value types vs. reference types

--- Boxing/Program.cs
+++ Boxing/Program.cs
@@ -7,15 +7,31 @@ namespace Boxing
         static void Main(string[] args)
         {
             int i = 42;
-
             object o = i;
             // object o = object(i); // explicit boxing
-
             i = 43;
-
             Console.WriteLine("Value typed int i: " + i);
             Console.WriteLine("Object typed object o: " + o);
+
+            A a = new A();
+            a.MyProperty = 42;
+            var b = a;
+            b.MyProperty = 99;
+            Console.WriteLine(a.MyProperty);
+
+            int myInt = 42;
+            int myInt2 = myInt;
+            myInt2 = 99;
+            Console.WriteLine(myInt);
+
+            string str = "str";
+            string str2 = str;
+            str2 = "hohohaha";
+            Console.WriteLine(str);
+
             Console.ReadKey();
         }
     }
+
+    class A { public int MyProperty { get; set; }}
 }
