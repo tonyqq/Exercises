a9631639b52745bc53bd2cc82c5b35d0d0de73e7
Anton Szórád <anton.szorad@gmail.com>
2015-11-06 15:28:36 +0100

    Comparing .ToList() vs. ToArray() speed

---
+++ ToArrayVsToListFSharp/Program.fs
@@ -0,0 +1,25 @@
+﻿    open System
+    open System.Collections.Generic
+    open System.Diagnostics
+    open System.Linq
+     
+//     Comparing .ToList() vs. ToArray() speed
+    let seqLength = 100000
+     
+    let items = seq { for i = 1 to seqLength do yield i }
+     
+    let runTest (name, f) =
+        let sw = Stopwatch()
+        sw.Start()
+        for i = 1 to 100 do f()
+        sw.Stop()
+        sw.ElapsedMilliseconds
+     
+    let tests = [| "ToList", (fun () -> items.ToList() |> ignore)
+                   "ToArray", (fun () -> items.ToArray() |> ignore) |]
+     
+    tests
+    |> Seq.map (fun (name, exec) -> name, runTest(name, exec))
+    |> Seq.iter (fun (name, timeMs) -> printfn "%8s  %Oms" name timeMs)
+
+    System.Console.ReadKey() |> ignore
